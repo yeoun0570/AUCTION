1cc74ffea7eefe9c071efe30bffb54f9dcad9b44
여운 <yeoun0570@gmail.com>
2024-10-21 17:26:50 +0900

    :sparkles: 프론트 서버 임시 작업

--- src/main/auction_front/src/App.vue
+++ src/main/auction_front/src/App.vue
@@ -1,58 +1,13 @@
-// src/App.vue
-<template>
-  <div id="app">
-    <h1>백엔드에서 받은 데이터:</h1>
-    <pre>{{ data }}</pre>
-  </div>
-</template>
-
 <script>
-import axios from "axios";
-import { ref, onMounted } from "vue";
+import ConnectTest from '@/components/ConnectTest.vue';
 
 export default {
-  setup() {
-    const data = ref("로딩 중..."); //데이터 받기 전 기본메시지
-
-    onMounted(async () => {
-      try {
-        const response = await axios.get("http://localhost:4000/api/test");
-        data.value = response.data;
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    });
-
-    return { data };
-  }
+  components: {
+    ConnectTest,
+  },
 };
 </script>
 
-
-
-<!-- <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template>
+  <ConnectTest/>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style> -->
---
+++ src/main/auction_front/src/components/ConnectTest.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const data = ref("로딩 중..."); //데이터 받기 전 기본메시지
+
+    onMounted(async () => {
+      try {
+        const response = await axios.get("http://localhost:4000/api/test");
+        data.value = response.data;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    });
+
+    return { data };
+  }
+};
+</script>
+
+<template>
+  <div id="app">
+    <h1>백엔드에서 받은 데이터:</h1>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
